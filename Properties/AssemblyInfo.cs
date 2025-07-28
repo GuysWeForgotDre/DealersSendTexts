3d6134833cd2132432b25bf6d4b8430a177c236c
dre <mynameismolotov@gmail.com>
2025-07-28 01:20:40 -0400

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: MelonInfo(typeof(DealersSendTexts.DealerText), "Dealers Send Texts", "1.0.0", "GuysWeForgotDre")]
+[assembly: MelonGame("TVGS", "Schedule I")]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Dealers Send Texts")]
+[assembly: AssemblyDescription("Dealers text updates on deals and daily summary in Schedule One")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Dealers Send Texts")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("51af033c-2fba-4fd9-950e-a5fb45e211c4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Source/Patches.cs
@@ -0,0 +1,110 @@
+﻿#if   Il2Cpp
+using Il2CppScheduleOne.Economy;
+using Il2CppScheduleOne.PlayerScripts;
+using Il2CppScheduleOne.Quests;
+using Il2CppScheduleOne.DevUtilities;
+using Il2CppScheduleOne.GameTime;
+using Il2CppScheduleOne.Messaging;
+
+#elif Mono
+using ScheduleOne.Economy;
+using ScheduleOne.PlayerScripts;
+using ScheduleOne.Quests;
+using ScheduleOne.DevUtilities;
+using ScheduleOne.GameTime;
+using ScheduleOne.Messaging;
+using System.Reflection;
+
+#endif
+using HarmonyLib;
+using MelonLoader;
+using UnityEngine.UI;
+
+namespace DealersSendTexts
+{
+    //Contracts Patches
+
+    [HarmonyPatch(typeof(Contract), nameof(Contract.InitializeContract))]
+    public class ContractInitializeContractPatch
+    {
+        static void Postfix(Contract __instance)
+        {
+            if (__instance is null || __instance.Dealer is null) return;
+            DealerStats.ProcessContract(__instance, EContractState.Started);
+        }
+    }
+
+    [HarmonyPatch(typeof(Contract), nameof(Contract.End))]
+    public class ContractEndPatch
+    {
+        static void Prefix(Contract __instance)
+        {
+            if (__instance is null || __instance.Dealer is null) return;
+            DealerStats.ProcessContract(__instance, EContractState.Failure);
+        }
+    }
+
+    [HarmonyPatch(typeof(Contract), nameof(Contract.Complete))]
+    public class ContractCompletePatch
+    {
+        static void Prefix(Contract __instance)
+        {
+            if (__instance is null || __instance.Dealer is null) return;
+
+            DealerStats.Completed.Add(__instance.Customer.GetComponent<Customer>()?.NPC?.fullName ?? "Unknown");
+            DealerStats.CheckProductAlerts(__instance.Dealer, Util.Prefix(__instance.DeliveryLocation.LocationName));
+        }
+    }
+
+    //Player Patches
+
+    [HarmonyPatch(typeof(Player), "SleepStart")]
+    public class PlayerSleepStartPatch
+    {
+        static void Prefix()
+        {
+            foreach (DealerStats stats in DealerStats.Dealers.Values)
+            {
+                stats.SendSummary();
+                stats.Sales.Clear();
+            }
+        }
+    }
+
+    //Dealer Patches
+
+    [HarmonyPatch(typeof(Dealer), "Start")]
+    public class DealerStartPatch
+    {
+        static void Postfix(Dealer __instance)
+        {
+            //if (__instance.IsRecruited)
+            DealerPrefs.AddDealer(__instance.FirstName);
+        }
+    }
+
+    // MSGConversation Patches
+
+    [HarmonyPatch(typeof(MSGConversation), "RefreshPreviewText")]
+    public class MSGConversationRefreshPreviewTextPatch
+    {
+        static void Postfix(MSGConversation __instance)
+        {
+            string day = NetworkSingleton<TimeManager>.Instance.CurrentDay.ToString();
+            int count  = __instance.bubbles.Count;
+#if Il2Cpp
+            if (count > 1 && __instance.entryPreviewText.text.StartsWith(day))
+                __instance.entryPreviewText.text = __instance.bubbles[count - 2].text;
+#elif Mono
+            FieldInfo field = AccessTools.Field(typeof(Text), "entryPreviewText");
+            string last = "";
+
+            if (field != null) 
+                last = field.GetValue(__instance).ToString();
+
+            if (count > 1 && last.StartsWith(day))
+                field.SetValue(__instance, __instance.bubbles[count - 2].text);
+#endif
+        }
+    }
+}
